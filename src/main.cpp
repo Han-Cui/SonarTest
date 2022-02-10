9dc15b5735d216a0fe79257845ff4cafc03cd79e
Han-Cui <32874764+Han-Cui@users.noreply.github.com>
2022-02-10 13:52:41 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
-void f(char *password, size_t bufferSize) {
-  char localToken[256];
-  init(localToken, password);
-  memset(password, ' ', strlen(password)); // Noncompliant, password is about to be freed
-  memset(localToken, ' ', strlen(localBuffer)); // Noncompliant, localToken is about to go out of scope
-  free(password);
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  int num = argc - 1;
+
+  if (num == 0) {
+    cout << "No arguments provided\n";
+  } else if (num == 0) { // intentional mistake
+    cout << "1 argument provided\n";
+  } else if (num == 2) {
+    cout << "2 arguments provided\n";
+  } else {
+    cout << num << " arguments provided\n";
+  }
+  if (argv != 0) {
+    cout << "argv not null\n";; // intentional extra-semicolon
+  }
+  if (argv == nullptr) {
+    return **argv; // intentional nullptr dereference
+  }
+
+  return 0;
 }
